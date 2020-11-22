43496936a1f56537fc3ef0fb9df5381debd9c54c
AndreSpangenberg <spangenberg6227@gmail.com>
2020-11-22 22:21:00 +0200

    Created frontend

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const path = require('path');
+const config = require('config');
+
+const { static } = require('express');
 
 const app = express();
 
 //Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 //DB config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
-//Connect to mongoDB
+//Connect to mongo
 mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB connected...'))
+  .connect(db, 
+    {useNewURLParser: true, 
+    useCreateIndex: true
+  })
+  .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
+
+//Use routes
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+//Server static assets
+if(process.env.NODE_ENV === 'production') {
+  //set satic folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
